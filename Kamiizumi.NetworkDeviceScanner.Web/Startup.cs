d95dbefe1488f701c624c92de713ac69af56c652
Ben Newman <ben@newman.ws>
2020-06-15 01:04:33 +0100

    Use PathBase for setting the base URL

--- Kamiizumi.NetworkDeviceScanner.Web/Startup.cs
+++ Kamiizumi.NetworkDeviceScanner.Web/Startup.cs
@@ -62,7 +62,7 @@
         /// <remarks>This method gets called by the runtime. Use this method to configure the HTTP request pipeline.</remarks>
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseForwardedHeaders();
+            app.UsePathBase(Configuration.GetValue<string>("PathBase"));
 
             if (env.IsDevelopment())
             {
